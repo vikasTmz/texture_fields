00972fb19ad92bb865e1e9f2a3e96dbe3cc058f8
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2020-11-25 22:19:15 -0500

    debugging randint error: 2

--- mesh2tex/data/fields.py
+++ mesh2tex/data/fields.py
@@ -154,7 +154,9 @@ class DepthImageVisualizeField(Field):
         self.imageio_kwargs = dict()
 
     def load(self, model_path, idx):
+        print("model_path : ", model_path)
         folder_img = os.path.join(model_path, self.folder_name_img)
+        print("folder_img : ", folder_img)
         files_img = glob.glob(os.path.join(folder_img, '*.%s' % self.extension_img))
         print("files_img : ", files_img)
         files_img.sort()
