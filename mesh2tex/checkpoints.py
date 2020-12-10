a596c36269d0aad010bcdb1b6b23c52e56482dc2
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2020-12-09 23:15:25 -0500

    updated checkpoint file

--- mesh2tex/checkpoints.py
+++ mesh2tex/checkpoints.py
@@ -44,16 +44,50 @@ class CheckpointIO(object):
         print(url)
         print('=> Loading checkpoint from url...')
         out_dict = model_zoo.load_url(url, progress=True)
+        out_file_dict = torch.load(filename)
+
+        filename = os.path.join(self.checkpoint_dir, 'model.pt')
+
+        out_file_dict["model_g"]["geometry_encoder.fc_pos.weight"] = out_dict["model"]["encoder.fc_pos.weight"]
+        out_file_dict["model_g"]["geometry_encoder.fc_pos.bias"] = out_dict["model"]["encoder.fc_pos.bias"]
+        out_file_dict["model_g"]["geometry_encoder.block_0.fc_0.weight"] = out_dict["model"]["encoder.block_0.fc_0.weight"]
+        out_file_dict["model_g"]["geometry_encoder.block_0.fc_0.bias"] = out_dict["model"]["encoder.block_0.fc_0.bias"]
+        out_file_dict["model_g"]["geometry_encoder.block_0.fc_1.weight"] = out_dict["model"]["encoder.block_0.fc_1.weight"]
+        out_file_dict["model_g"]["geometry_encoder.block_0.fc_1.bias"] = out_dict["model"]["encoder.block_0.fc_1.bias"]
+        out_file_dict["model_g"]["geometry_encoder.block_0.shortcut.weight"] = out_dict["model"]["encoder.block_0.shortcut.weight"]
+        out_file_dict["model_g"]["geometry_encoder.block_1.fc_0.weight"] = out_dict["model"]["encoder.block_1.fc_0.weight"]
+        out_file_dict["model_g"]["geometry_encoder.block_1.fc_0.bias"] = out_dict["model"]["encoder.block_1.fc_0.bias"]
+        out_file_dict["model_g"]["geometry_encoder.block_1.fc_1.weight"] = out_dict["model"]["encoder.block_1.fc_1.weight"]
+        out_file_dict["model_g"]["geometry_encoder.block_1.fc_1.bias"] = out_dict["model"]["encoder.block_1.fc_1.bias"]
+        out_file_dict["model_g"]["geometry_encoder.block_1.shortcut.weight"] = out_dict["model"]["encoder.block_1.shortcut.weight"]
+        out_file_dict["model_g"]["geometry_encoder.block_2.fc_0.weight"] = out_dict["model"]["encoder.block_2.fc_0.weight"]
+        out_file_dict["model_g"]["geometry_encoder.block_2.fc_0.bias"] = out_dict["model"]["encoder.block_2.fc_0.bias"]
+        out_file_dict["model_g"]["geometry_encoder.block_2.fc_1.weight"] = out_dict["model"]["encoder.block_2.fc_1.weight"]
+        out_file_dict["model_g"]["geometry_encoder.block_2.fc_1.bias"] = out_dict["model"]["encoder.block_2.fc_1.bias"]
+        out_file_dict["model_g"]["geometry_encoder.block_2.shortcut.weight"] = out_dict["model"]["encoder.block_2.shortcut.weight"]
+        out_file_dict["model_g"]["geometry_encoder.block_3.fc_0.weight"] = out_dict["model"]["encoder.block_3.fc_0.weight"]
+        out_file_dict["model_g"]["geometry_encoder.block_3.fc_0.bias"] = out_dict["model"]["encoder.block_3.fc_0.bias"]
+        out_file_dict["model_g"]["geometry_encoder.block_3.fc_1.weight"] = out_dict["model"]["encoder.block_3.fc_1.weight"]
+        out_file_dict["model_g"]["geometry_encoder.block_3.fc_1.bias"] = out_dict["model"]["encoder.block_3.fc_1.bias"]
+        out_file_dict["model_g"]["geometry_encoder.block_3.shortcut.weight"] = out_dict["model"]["encoder.block_3.shortcut.weight"]
+        out_file_dict["model_g"]["geometry_encoder.block_4.fc_0.weight"] = out_dict["model"]["encoder.block_4.fc_0.weight"]
+        out_file_dict["model_g"]["geometry_encoder.block_4.fc_0.bias"] = out_dict["model"]["encoder.block_4.fc_0.bias"]
+        out_file_dict["model_g"]["geometry_encoder.block_4.fc_1.weight"] = out_dict["model"]["encoder.block_4.fc_1.weight"]
+        out_file_dict["model_g"]["geometry_encoder.block_4.fc_1.bias"] = out_dict["model"]["encoder.block_4.fc_1.bias"]
+        out_file_dict["model_g"]["geometry_encoder.block_4.shortcut.weight"] = out_dict["model"]["encoder.block_4.shortcut.weight"]
+        out_file_dict["model_g"]["geometry_encoder.fc_c.weight"] = out_dict["model"]["encoder.fc_c.weight"]
+        out_file_dict["model_g"]["geometry_encoder.fc_c.bias"] = out_dict["model"]["encoder.fc_c.bias"]
+
         # scalars = self.parse_state_dict(state_dict)
         for k, v in self.module_dict.items():
             print("Start loading: %s" % k)
-            if k in out_dict:
+            if k in out_file_dict:
                 # print(out_dict[k])
-                v.load_state_dict(out_dict[k])
+                v.load_state_dict(out_file_dict[k])
                 print("Finished: %s" % k)
             else:
                 print('Warning: Could not find %s in checkpoint!' % k)
-        scalars = {k: v for k, v in out_dict.items()
+        scalars = {k: v for k, v in out_file_dict.items()
                     if k not in self.module_dict}
         return scalars
 
